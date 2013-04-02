d6b32e6b8ab6bd0d7c0b3dd7176cef8d4571b6b7
Bas van der Hoek <bas@hoek.com>
2013-04-02 15:36:32 +0200

    fitnesse login test with fixture

---
+++ src/test/java/com/hoek/tn5250/FitNesseFixture.java
@@ -0,0 +1,11 @@
+package com.hoek.tn5250;
+
+public class FitNesseFixture {
+
+    public FitNesseFixture() {
+    }
+
+    public boolean loginWithUsernameAndPassword(String username, String password) {
+        return true;
+    }
+}
